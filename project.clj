c272fd8bbcc8bcdcb6b5ad1482225742b0287379
matthewlisp <matthewlisp@gmail.com>
2019-11-05 13:42:55 -0300

    adds ring dependency

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/matthewlisp/URL_shortener"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [ring/ring-core "1.7.1"]]
   :main ^:skip-aot url-shortener.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
