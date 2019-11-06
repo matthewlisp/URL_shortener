1f5851aff1b2f182e1c0f8e5d19f617d2684cc76
matthewlisp <matthewlisp@gmail.com>
2019-11-06 13:46:09 -0300

    creates compojure routes

--- src/url_shortener/core.clj
+++ src/url_shortener/core.clj
@@ -1,10 +1,11 @@
 (ns url-shortener.core
-  (:require [ring.adapter.jetty   :refer [run-jetty]]
-            [ring.middleware.json :refer [wrap-json-response]]
-            [ring.util.response   :refer [response redirect]]
-            [compojure.core       :refer [routes GET POST]]
-            [compojure.route      :refer [not-found]]
-            [compojure.coercions  :refer [as-int]])
+  (:require [ring.adapter.jetty     :refer [run-jetty]]
+            [ring.middleware.json   :refer [wrap-json-response]]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.util.response     :refer [response redirect]]
+            [compojure.core         :refer [routes GET POST]]
+            [compojure.route        :refer [not-found]]
+            [compojure.coercions    :refer [as-int]])
   (:gen-class))
 
 
@@ -12,11 +13,9 @@
 
 (def uri-regex  #"\w+:(\/?\/?)[^\s]+")
 
-(def number-regex #"^[0-9]*$")
-
 (def short-url-resp {:original_url nil :short_url nil})
 
-(def short-url-err {:error "invalid URL or malformed header"})
+(def short-url-err {:error "invalid URL"})
 
 
 ;;; STORAGE
@@ -36,30 +35,17 @@
 
 ;; API
 
-
-(defn remove-slash
-  [uri]
-  (clojure.string/replace uri #"/" ""))
-
-(defn handler
-  [{{:strs [uri]} :headers :as req}]
-  (let [short-url (keyword (remove-slash (:uri req)))]
-    (if (contains? @uris short-url)
-      (short-url @uris)
-      (if (and (= (:uri req) "shorturl")
-               (= (:request-method req) :post)
-               (re-matches uri-regex uri))
-        (response {:original_url uri
-                   :short_url (shorten-uri uri)})
-        (response {:error "invalid URL or malformed header"})))))
-
 (def my-routes
   (routes
-   (POST "/shorturl"  [address]              (fn [x] (str "REDIRECT TO " address)))
-   (GET  "/:shorturl" [shorturl :<< as-int] #(if (contains? @uris %) ((keyword %) @uris) short-url-err))
-   (not-found {:error "Not found"})))
+   (POST  "/shorturl"  [address]             (fn [_] (if (re-matches uri-regex address) (response (assoc short-url-resp :original_url address :short_url (shorten-uri address))) (response short-url-err))))
+   (GET   "/:shorturl" [shorturl :<< as-int] (fn [_] (if (contains? @uris shorturl) (get @uris shorturl) (response short-url-err))))
+   (not-found (response {:error "Not found"}))))
 
+(def app 
+  (-> my-routes
+      wrap-params
+      wrap-json-response))
 
 (defn -main
   []
-  (run-jetty (wrap-json-response my-routes) {:port 3000}))
+  (run-jetty app {:port 3000}))
